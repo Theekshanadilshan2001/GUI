ad80410454750b603b74825456026c16885f36f5
Theekshanadilshan2001 <theekshanadilshanapply2021@gmali.com>
2025-01-11 16:42:44 +0530

    Footer section css code

--- new/obj/Debug/net8.0/new.AssemblyInfo.cs
+++ new/obj/Debug/net8.0/new.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4dbdeed4eaecb9dce69c28a26d8b2d8dbdc154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6668351a8bc4df85303eafe5cfa24aaadd928579")]
 [assembly: System.Reflection.AssemblyProductAttribute("new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
