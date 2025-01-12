c56bff25d4f8692295a3e789e1c10112d1d97d24
Theekshanadilshan2001 <theekshanadilshanapply2021@gmali.com>
2025-01-12 21:00:38 +0530

    Give adviise for user

--- new/obj/Debug/net8.0/new.AssemblyInfo.cs
+++ new/obj/Debug/net8.0/new.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f27f2c4c4931e644d1bf878d5f683d66966e1fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e0f9bd782e94d9e67483b85f8f142908d37528")]
 [assembly: System.Reflection.AssemblyProductAttribute("new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
